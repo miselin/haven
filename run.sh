d1bd75b8e660f15df58fc4903a254ec4a308b1f8
Matt Iselin <matthew@theiselins.net>
2024-12-13 18:24:27 -0800

    update run script for command line flags

--- run.sh
+++ run.sh
@@ -3,13 +3,10 @@
 set -e
 set -o pipefail
 
-CFLAGS="-O2 -g -gdwarf-2 -fstandalone-debug -fno-eliminate-unused-debug-types -gline-tables-only"
+CFLAGS="-O1 -g -gdwarf-2 -fstandalone-debug -fno-eliminate-unused-debug-types -gline-tables-only"
 
-gcc -E - <$1 >$1.preproc
-./build/bin/mattc $1.preproc $1.ll 2>&1 | tee log.log
-
-clang-18 -c ${CFLAGS} -o $1.o -x ir $1.ll
-clang-18 -S ${CFLAGS} -emit-llvm -o $1.opt.ll $1.ll
-objdump -S $1.o >$1.s
-
-clang-18 ${CFLAGS} -o $1.bin $1.o -lm
+./build/bin/mattc --verbose --emit-ir $1 2>&1 | tee log.log
+clang-18 -O2 -c -o ${1%.*}.o ${1%.*}.ll
+clang-18 -O2 -S -emit-llvm -o ${1%.*}.opt.ll ${1%.*}.ll
+objdump -S ${1%.*}.o >${1%.*}.s
+clang-18 -o ${1%.*} ${1%.*}.o -lm
