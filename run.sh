8c5ce49849d2a9451de335e423981bd6e26f6866
Matt Iselin <matthew@theiselins.net>
2024-12-14 21:56:16 -0800

    avoid appending to log in run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 
-./build/bin/haven --verbose --debug-ast --emit-ir $1 2> >(tee -a log.log >&2)
+./build/bin/haven --verbose --debug-ast --emit-ir $1 2> >(tee log.log >&2)
 clang-18 -O2 -c -o ${1%.*}.o ${1%.*}.ll
 clang-18 -O2 -S -emit-llvm -o ${1%.*}.opt.ll ${1%.*}.ll
 objdump -S ${1%.*}.o >${1%.*}.s
